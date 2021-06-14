8d81e96844b39cd65fe944fe3d64207620c39486
unknown <naikpallavi005@gmail.com>
2021-06-14 18:03:41 +0530

    first commit

---
+++ src/components/TextForm.js
@@ -0,0 +1,34 @@
+class TextForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Please write about your favorite DOM element.'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log('An essay was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Essay:
+          <textarea value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default TextForm;
